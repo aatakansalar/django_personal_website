c425fd2af4b23cc26c70ff114c4b659ec1e1ee8d
Atakan Salar <51931451+aatakansalar@users.noreply.github.com>
2020-09-22 02:33:53 +0300

    Update settings.py

--- personal_website/settings.py
+++ personal_website/settings.py
@@ -81,9 +81,9 @@ WSGI_APPLICATION = 'personal_website.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": str(BASE_DIR / "db.sqlite3"),
     }
 }
 
